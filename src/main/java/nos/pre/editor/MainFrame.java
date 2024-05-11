7b52f67b09a345622a975fb25424b80c710c412c
nighthowlrr <75522658+nighthowlrr@users.noreply.github.com>
2024-05-12 03:55:28 +0500

    Add line numbers

--- src/main/java/nos/pre/editor/MainFrame.java
+++ src/main/java/nos/pre/editor/MainFrame.java
@@ -1,15 +1,39 @@
 package nos.pre.editor;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+
+    private final JPanel contentPanel = new JPanel(new BorderLayout(), true);
     public MainFrame() {
         super("PreEditor");
         setSize(1000, 700);
+        setResizable(false);
         setLocationRelativeTo(null);
-
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        contentPanel.setOpaque(true);
+        contentPanel.setBackground(Color.GRAY);
+        setContentPane(contentPanel);
+
+        addUIComponents();
+
         setVisible(true);
+    }
+
+    private void addUIComponents() {
+        JPanel textPanel = new JPanel(new BorderLayout(), true);
+
+        JTextPane textPane = new JTextPane();
+        textPane.setFont(new Font("League Spartan", Font.PLAIN, 22));
+        textPanel.add(textPane, BorderLayout.CENTER);
+
+        JTextLineNumber textLineNumber = new JTextLineNumber(textPane);
+        textPanel.add(textLineNumber, BorderLayout.WEST);
+
+        JScrollPane scrollPane = new JScrollPane(textPanel);
+        scrollPane.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 10));
+        contentPanel.add(scrollPane, BorderLayout.CENTER);
     }
 }
