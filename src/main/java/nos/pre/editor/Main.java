dbe5cd9c53f9d9fcda08bc33d3e52b5aa9d509cf
nighthowlrr <75522658+nighthowlrr@users.noreply.github.com>
2024-05-11 12:49:07 +0500

    Initial commit

---
+++ src/main/java/nos/pre/editor/Main.java
@@ -0,0 +1,7 @@
+package nos.pre.editor;
+
+public class Main {
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+}
---
+++ src/main/java/nos/pre/editor/MainFrame.java
@@ -0,0 +1,15 @@
+package nos.pre.editor;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        super("PreEditor");
+        setSize(1000, 700);
+        setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+}
