748f03df3a4270878b4f4b451dfd9eb8ccae04f2
nighthowlrr <75522658+nighthowlrr@users.noreply.github.com>
2024-05-12 16:57:35 +0500

    Update MainFrame.java

--- src/main/java/nos/pre/editor/MainFrame.java
+++ src/main/java/nos/pre/editor/MainFrame.java
@@ -1,11 +1,16 @@
 package nos.pre.editor;
 
+import templateUI.JTextLineNumber;
+import templateUI.jScrollPane;
+
 import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
 
-    private final JPanel contentPanel = new JPanel(new BorderLayout(), true);
+    private final JPanel mainContentPanel = new JPanel(new BorderLayout(), true);
     public MainFrame() {
         super("PreEditor");
         setSize(1000, 700);
@@ -13,9 +18,7 @@ public class MainFrame extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        contentPanel.setOpaque(true);
-        contentPanel.setBackground(Color.GRAY);
-        setContentPane(contentPanel);
+        setContentPane(mainContentPanel);
 
         addUIComponents();
 
@@ -23,17 +26,55 @@ public class MainFrame extends JFrame {
     }
 
     private void addUIComponents() {
-        JPanel textPanel = new JPanel(new BorderLayout(), true);
+        mainContentPanel.add(new EditorView(), BorderLayout.CENTER);
+    }
 
-        JTextPane textPane = new JTextPane();
-        textPane.setFont(new Font("League Spartan", Font.PLAIN, 22));
-        textPanel.add(textPane, BorderLayout.CENTER);
+    private static class EditorView extends JPanel {
+        private final jScrollPane scrollPane = new jScrollPane();
+        private final JPanel textContentPanel = new JPanel(new BorderLayout(), true);
+        private final JTextPane textPane = new JTextPane();
+        private final JTextLineNumber textLineNumber = new JTextLineNumber(textPane);
 
-        JTextLineNumber textLineNumber = new JTextLineNumber(textPane);
-        textPanel.add(textLineNumber, BorderLayout.WEST);
+        private final JPanel bottomPanel = new JPanel(new BorderLayout(), true);
+        private final JLabel caretLocationLabel = new JLabel(String.valueOf(textPane.getCaretPosition()));
 
-        JScrollPane scrollPane = new JScrollPane(textPanel);
-        scrollPane.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 10));
-        contentPanel.add(scrollPane, BorderLayout.CENTER);
+        public EditorView() {
+            super(new BorderLayout(), true);
+
+            this.addTextContent();
+            this.addBottomPanel();
+        }
+
+        private void addTextContent() {
+            textPane.setBackground(new Color(0x1e1f22));
+            textPane.setForeground(Color.WHITE);
+            textPane.setFont(new Font("League Spartan", Font.PLAIN, 22));
+            textPane.addCaretListener(new CaretListener() {
+                @Override
+                public void caretUpdate(CaretEvent e) {
+                    caretLocationLabel.setText(String.valueOf(textPane.getCaretPosition()));
+                }
+            });
+            textContentPanel.add(textPane, BorderLayout.CENTER);
+
+            textLineNumber.setCurrentLineForeground(Color.WHITE);
+            textContentPanel.add(textLineNumber, BorderLayout.WEST);
+
+
+            scrollPane.setViewportView(textContentPanel);
+            scrollPane.setBorder(BorderFactory.createLineBorder(new Color(0x2b2d30), 2));
+            this.add(scrollPane, BorderLayout.CENTER);
+        }
+
+        private void addBottomPanel() {
+            bottomPanel.setPreferredSize(new Dimension(0, 20));
+            bottomPanel.setBackground(new Color(0x2b2d30));
+
+            caretLocationLabel.setBorder(BorderFactory.createLineBorder(bottomPanel.getBackground(), 2));
+            caretLocationLabel.setForeground(Color.LIGHT_GRAY);
+
+            bottomPanel.add(caretLocationLabel);
+            this.add(bottomPanel, BorderLayout.SOUTH);
+        }
     }
 }
