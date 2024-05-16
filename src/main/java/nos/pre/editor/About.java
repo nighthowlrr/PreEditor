df8e81ee0fb10c77d1fc2f25a2a488f686195a36
nighthowlrr <75522658+nighthowlrr@users.noreply.github.com>
2024-05-16 09:35:13 +0500

    Add About with version

---
+++ src/main/java/nos/pre/editor/About.java
@@ -0,0 +1,9 @@
+package nos.pre.editor;
+
+public class About {
+    public static final String appName = "PreEditor";
+    public static final String fullAppName = "Nos PreEditor";
+    public static final String developer = "Nos";
+
+    public static final String version = "0.0.0";
+}
