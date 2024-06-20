a3fe836f76fc2210864d8cd58723382714c25ede
nighthowlrr <75522658+nighthowlrr@users.noreply.github.com>
2024-06-21 01:15:41 +0500

    Add some template completions

--- src/main/java/nos/pre/editor/languages/java/JavaCompletions.java
+++ src/main/java/nos/pre/editor/languages/java/JavaCompletions.java
@@ -1,12 +1,25 @@
 package nos.pre.editor.languages.java;
 
 import nos.pre.editor.autoComplete.completions.CompletionList;
+import nos.pre.editor.autoComplete.completions.TemplateCompletion;
 
-import java.util.ArrayList;
-
-public class JavaCompletions extends ArrayList<BaseCompletion> {
 public class JavaCompletions extends CompletionList {
     public JavaCompletions() {
         this.addAll(JavaKeywords.getKeywordCompletions());
+
+        this.add(new TemplateCompletion("fori",
+                "for (int i = 0; i < ", "; i++) {}",
+                "Create iteration \"for\" loop"));
+        // TODO: Check if variable i is used, if yes, then use j ...
+
+        this.add(new TemplateCompletion("sout", "System.out.println(", ");",
+                "Print a string to System.out"));
+
+        this.add(new TemplateCompletion("main",
+                "public static void main(String[] args) {", "}",
+                "main() method declaration"));
+        this.add(new TemplateCompletion("psvm",
+                "public static void main(String[] args) {", "}",
+                "main() method declaration"));
     }
 }
