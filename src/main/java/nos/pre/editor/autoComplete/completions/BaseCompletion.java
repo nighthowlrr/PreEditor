9df98ce2eaa54e4bd2ceb979d0d568f7c7f1d422
nighthowlrr <75522658+nighthowlrr@users.noreply.github.com>
2024-06-21 01:12:10 +0500

    Small changes

--- src/main/java/nos/pre/editor/autoComplete/completions/BaseCompletion.java
+++ src/main/java/nos/pre/editor/autoComplete/completions/BaseCompletion.java
@@ -6,19 +6,20 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 
 public class BaseCompletion implements Comparable<BaseCompletion> {
-    private static final int DEFAULT_RELEVANCE = 0;
+    public static final int DEFAULT_RELEVANCE = 0;
+    private static final String DEFAULT_SUMMARY = null;
 
     private final String completionText;
     private final String summary;
     private final int relevance;
 
     public BaseCompletion(String completionText) {
-        this(completionText, null, DEFAULT_RELEVANCE);
+        this(completionText, DEFAULT_SUMMARY, DEFAULT_RELEVANCE);
     }
     public BaseCompletion(String completionText, String summary) {
         this(completionText, summary, DEFAULT_RELEVANCE);
     }
-    public BaseCompletion(String completionText, String summary, int relevance) {
+    public BaseCompletion(@NotNull String completionText, @Nullable String summary, int relevance) {
         this.completionText = completionText;
         this.summary = summary;
         this.relevance = relevance;
@@ -35,6 +36,17 @@ public class BaseCompletion implements Comparable<BaseCompletion> {
         } else {
             // return c2 != null ? this.toString().compareToIgnoreCase(c2.toString()) : -1;
             return this.toString().compareToIgnoreCase(c2.toString());
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.summary != null) {
+            return "{" + this.getClass().getSimpleName() + ": completionText: " + this.completionText +
+                    ", summary: " + this.summary + ", relevance: " + this.relevance + "}";
+        } else {
+            return "{" + this.getClass().getSimpleName() + ": completionText: " + this.completionText +
+                    ", relevance: " + this.relevance + "}";
         }
     }
 
