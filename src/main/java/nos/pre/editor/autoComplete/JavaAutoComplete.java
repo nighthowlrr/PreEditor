919a08c7724f4054f5f38c1e6627fad9bf21956a
nighthowlrr <75522658+nighthowlrr@users.noreply.github.com>
2024-07-13 05:31:57 +0500

    Add Java auto-complete
    (Class methods only. Not context-specific)

---
+++ src/main/java/nos/pre/editor/autoComplete/JavaAutoComplete.java
@@ -0,0 +1,23 @@
+package nos.pre.editor.autoComplete;
+
+import nos.pre.editor.autoComplete.completions.CompletionList;
+import nos.pre.editor.coderead.JavaCodeRead;
+import nos.pre.editor.languages.java.JavaCompletions;
+import nos.pre.editor.pretextpane.PreTextPane;
+import org.jetbrains.annotations.NotNull;
+
+public class JavaAutoComplete extends AutoComplete {
+    public JavaAutoComplete(PreTextPane preTextPane, JavaCodeRead codeRead) {
+        super(preTextPane, new JavaCompletions(), codeRead);
+    }
+
+    @Override
+    protected @NotNull CompletionList getAllMatchingCompletions(String subWord) {
+        JavaCodeRead javaCodeRead = (JavaCodeRead) this.getCodeRead();
+
+        CompletionList matchingBasicCompletions = this.getBasicCompletions().getMatchingCompletions(subWord);
+        CompletionList matchingMethodCompletions = javaCodeRead.getClassMethodCompletions().getMatchingCompletions(subWord);
+
+        return CompletionList.combineAndSortCompletions(matchingBasicCompletions, matchingMethodCompletions);
+    }
+}
